b8d261fe0e22ac5f019b97908f7fef84a6b94426
qxzg <out@qxzg.xyz>
2020-02-20 09:18:59 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,6 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/fast_open = 0/fast_open = 1/g' package/lean/luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
+sed -i '10c\ulimit -n 1000000 && ulimit -p 65000' package/lean/luci-app-ssr-plus/root/etc/init.d/shadowsocksr
+sed -i 's/"Value=${__IP}"/"Value=${__IP}" "TTL=60"/g' package/lean/ddns-scripts_aliyun/update_aliyun_com.sh
